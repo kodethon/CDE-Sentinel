22d3a93c489169ac01036bdbf91dc6f8c574bbd5
Michael Yen <lvlichael8@gmail.com>
2018-02-21 01:22:22 -0800

    Run retab on constants.rb

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,26 +1,25 @@
 class Constants
+  def self.app 
+    return {
+      :MASTER_IP_ADDR => ENV['MASTER_IP_ADDR'],
+      :MASTER_PORT => ENV['MASTER_PORT'],
+      :SYNC_FOLDER => 'sync'
+    }
+  end
 
-	def self.app 
-		return {
-			:MASTER_IP_ADDR => ENV['MASTER_IP_ADDR'],
-			:MASTER_PORT => ENV['MASTER_PORT']
-		}
-	end
+  def self.cache
+    return {
+      :FC_ACCESS => 'fc.access',
+      :ENV_ACCESS => 'env.access',
+      :LAST_ACCESS => '-modified',
+      :AVAILABLE_DISK => 'available-disk'
+    }
+  end
 
-	def self.cache
-		return {
-		    :FC_ACCESS => 'fc.access',
-		    :ENV_ACCESS => 'env.access',
-			:LAST_ACCESS => '-modified',
-			:AVAILABLE_DISK => 'available-disk'
-		}
-	end
-
-	def self.host 
-		return {
-			:IP_ADDR => ENV['HOST_IP_ADDR'],
-			:PORT => ENV['HOST_PORT'],
-		}
-	end
-
+  def self.host 
+    return {
+      :IP_ADDR => ENV['HOST_IP_ADDR'],
+      :PORT => ENV['HOST_PORT'],
+    }
+  end
 end
