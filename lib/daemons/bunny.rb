a3e6da119ddfa3ceff82dcab1ef5ba661244995c
Michael Yen <lvlichael8@gmail.com>
2018-03-17 02:23:20 -0700

    Update bunny daemon

--- lib/daemons/bunny.rb
+++ lib/daemons/bunny.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'bunny'
+require 'fileutils'
 
 # You might want to change this
 ENV["RAILS_ENV"] ||= "development"
@@ -11,35 +12,35 @@ Dir.chdir(root)
 require File.join(root, "config", "environment")
 require File.join(root, 'lib', 'env.rb')
 
+conn = Bunny.new("amqp://%s:%s" % [Env.instance['RABBITMQ_IP_ADDR'], Env.instance['RABBITMQ_PORT']])
+conn.start
+ch = conn.create_channel
+
+# Add root public key
+q  = ch.queue(Constants.rabbitmq[:CHANNELS][:ROOT_PUBLIC_KEY], :auto_delete => true)
+q.subscribe do |delivery_info, metadata, payload|
+  authorized_keys = File.join('/root/.ssh/authorized_keys')
+  Rails.logger.info '%s exists?' % authorized_keys
+  FileUtils.touch authorized_keys if not File.exists?(authorized_keys)
+
+  Rails.logger.info 'Opening %s' % authorized_keys
+  fp = File.open(authorized_keys, 'a+')
+  contents = fp.read
+
+  Rails.logger.info 'Payload included?'
+  if not contents.include? payload
+    Rails.logger.info "Appending %s" % authorized_keys
+    fp.write payload
+    fp.close
+  end
+end
+
 $running = true
 Signal.trap("TERM") do 
   $running = false
+  conn.close
 end
 
 while($running) do
-  begin
-    conn = Bunny.new("amqp://%s:%s" % [Env.instance['RABBITMQ_IP_ADDR'], Env.instance['RABBITMQ_PORT']])
-    conn.start
-
-    ch = conn.create_channel
-
-    # Add root public key
-    q  = ch.queue(Constants.rabbitmq[:CHANNELS][:ROOT_PUBLIC_KEY], :auto_delete => true)
-    q.subscribe do |delivery_info, metadata, payload|
-      authorized_keys = '/root/.ssh/authorized_keys'
-      FileUtils.touch authorized_keys if not File.exists(authorized_keys)
-      fp = File.open(authorized_keys, 'a+')
-      contents = fp.read
-      if not contents.include? payload
-        Rails.logger.info "Updating %s" % authorized_keys
-        fp.write payload
-      end
-      fp.close
-    end
-
-    sleep 10
-    conn.close
-  rescue => err
-    Rails.logger.error err
-  end
+  sleep 10
 end
