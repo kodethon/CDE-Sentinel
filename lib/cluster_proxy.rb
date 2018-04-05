ac2f531f571e4d0a3937ad59f1693423b5802ba6
Michael Yen <lvlichael8@gmail.com>
2018-04-05 02:36:30 -0700

    Update resource usage calculations

--- lib/cluster_proxy.rb
+++ lib/cluster_proxy.rb
@@ -230,17 +230,13 @@ module ClusterProxy
     def self.get_resource_usage
       snapshot = Vmstat.snapshot
       disk = Vmstat.disk(Env.instance['NODE_DRIVES'])
-      cpu_idle = 0
-
-      for c in Vmstat.cpu
-        cpu_idle += c.idle
-      end
-
+      memory = snapshot.memory.free * snapshot.memory.pagesize / 1_000_000
+      memory = 1 if memory < 1
       {
         containers: Docker::Container.all.length - 1,
-        cpu: Math.sqrt(cpu_idle),
+        cpu: snapshot.cpus.length / snapshot.load_average.five_minutes,
         disk: disk.available_blocks * disk.block_size / 1_000_000,
-        memory: snapshot.memory.free * snapshot.memory.pagesize / 1_000_000
+        memory: Math.log(memory)
       }
     end
   end
