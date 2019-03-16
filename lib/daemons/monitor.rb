8e33329e2fe94024298640fbae37bfeb23d952c4
Michael Yen <lvlichael8@gmail.com>
2019-03-16 12:13:17 -0700

    Update process not killed handling

--- lib/daemons/monitor.rb
+++ lib/daemons/monitor.rb
@@ -38,6 +38,7 @@ while($running) do
 
   # Get top highest CPU using processes
   stdout, stderr, status = Open3.capture3('ps -eo pcpu,user,pid,etimes,command | sort -k1 -r -n | head -10')
+
   rows = stdout.split("\n")
   rows.each do |row|
     columns = row.strip().split(' ')
@@ -54,31 +55,31 @@ while($running) do
       within_docker = within_docker?(processes, super_key1) 
         
       # If it is within a container, check if it has abnormal CPU usage
-      if within_docker
-        Rails.logger.info 'Abnormal process within docker container found...'
+      next if not within_docker
+      Rails.logger.info 'Abnormal process within docker container found...'
 
-        active_time = columns[3].to_i
-        high_cpu = (active_time >= 15 && cpu_percent > 75)
-        medium_cpu = (active_time >= 30 && cpu_percent > 30)
-        low_cpu = (active_time >= 75 && cpu_percent > 15)
-        if high_cpu || medium_cpu || low_cpu 
-          Rails.logger.info "Abnormal CPU usage process found, killing..."
-          Rails.logger.info columns.join(' ')
-          puts "Abnormal CPU usage process found, killing..."
-          puts columns.join(' ')
+      active_time = columns[3].to_i
+      h_cpu = (active_time >= 15 && cpu_percent > 75)
+      m_cpu = (active_time >= 30 && cpu_percent > 30)
+      l_cpu = (active_time >= 75 && cpu_percent > 15)
+      if h_cpu || m_cpu || l_cpu 
+        Rails.logger.info "Abnormal CPU usage process found, killing..."
+        Rails.logger.info columns.join(' ')
+        puts "Abnormal CPU usage process found, killing..."
+        puts columns.join(' ')
 
-          stdout, stderr, status = Open3.capture3('sudo kill -9 ' + pid)
-          if status.exitstatus != 0 # If not success
-            stdout, stderr, status = Open3.capture3(
-              "docker ps | awk '{print $1}' | xargs docker inspect -f '{{ .State.Pid }} {{ .Config.Hostname }}'  | grep %s | awk '{print $2}'" % pid)
-            if status.exitstatus == 0
-              Rails.logger.info "Killing container %s" % stdout
-              stdout, stderr, status = Open3.capture3("docker kill %s" % stdout)
-              Rails.logger.info "%s to kill the container..." % status.exitstatus == 0 ? 'Succeeded' : 'Failed'
-            end
-          end # if
-        end # if
-      end
+        stdout, stderr, status = Open3.capture3('sudo kill -9 ' + pid)
+
+        # See if process was killed
+        stdout, stderr, status = Open3.capture3('ps -aux | grep %s | awk {print $2}' % pid)
+        rows = stdout.split("\n")
+        next if pid not in rows
+        Rails.logger.info "Process still alive, killing parent..."
+
+        # If the proces was not killed, kill its parent
+        # This should end up killing the container as a last resort
+        stdout, stderr, status = Open3.capture3('ps -o ppid= -p %s | xargs sudo kill -9 ' % pid)
+      end # if
     end # if precent > 15
   end
   
